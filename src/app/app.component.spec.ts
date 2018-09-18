ee4ce83977a1b1ce671195384ab1b5381b58e0b2
Maxime <maximep@stud.ntnu.no>
2018-09-18 10:36:39 +0200

    Code formating and firebase config tweaks

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,6 +1,6 @@
 import { TestBed, async } from '@angular/core/testing';
 import { AppComponent } from './app.component';
-import {Component, NO_ERRORS_SCHEMA} from "@angular/core";
+import {Component, NO_ERRORS_SCHEMA} from '@angular/core';
 
 @Component({ selector: 'app-header', template: ''})
 class HeaderStubComponent {}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,18 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-import { HttpClientModule } from "@angular/common/http";
+import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
-import {FormsModule, ReactiveFormsModule} from "@angular/forms";
-import {RouterModule, Routes} from "@angular/router";
+import {FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {RouterModule, Routes} from '@angular/router';
 import { SignupComponent } from './auth/signup/signup.component';
 import { SigninComponent } from './auth/signin/signin.component';
 import { MapViewComponent } from './map-view/map-view.component';
 import { UploadComponent } from './upload/upload.component';
-import {AuthService} from "./services/auth.service";
-import {AuthGuardService} from "./services/auth-guard.service";
-import {MapViewService} from "./services/map-view.service";
+import {AuthService} from './services/auth.service';
+import {AuthGuardService} from './services/auth-guard.service';
+import {MapViewService} from './services/map-view.service';
 
 const appRoutes: Routes = [
   { path: 'auth/signup', component: SignupComponent},
