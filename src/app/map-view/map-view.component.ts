30c1da15307d99386b8da3fb97c8c36b7c833730
Maxime <maximep@stud.ntnu.no>
2018-09-27 14:42:15 +0200

    GLIDE-11 GLIDE-41 GLIDE-43 GLIDE-45 GLIDE-47 #time 4h #comment Finished working on the map-view service which handle the creation and the loading of a track on a map

--- src/app/map-view/map-view.component.ts
+++ src/app/map-view/map-view.component.ts
@@ -1,15 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-import Feature from 'ol/Feature';
-import Map from 'ol/Map';
-import View from 'ol/View';
-import {defaults as defaultControls} from 'ol/control';
-import IGC from 'ol/format/IGC';
-import {LineString, Point} from 'ol/geom';
-import {Tile as TileLayer, Vector as VectorLayer} from 'ol/layer';
-import OSM, {ATTRIBUTION} from 'ol/source/OSM';
-import VectorSource from 'ol/source/Vector';
-import {fromLonLat} from 'ol/proj';
-import {Circle as CircleStyle, Fill, Stroke, Style} from 'ol/style';
+import {MapViewService} from '../services/map-view.service';
+import {Subscription} from 'rxjs';
 
 @Component({
   selector: 'app-map-view',
@@ -21,183 +12,34 @@ export class MapViewComponent implements OnInit {
   // Test urls of IGC files
   igcUrls =  [
     'http://openlayers.org/en/latest/examples/data/igc/Clement-Latour.igc',
-    'http://openlayers.org/en/latest/examples/data/igc/Damien-de-Baenst.igc',
-    'http://openlayers.org/en/latest/examples/data/igc/Tom-Payne.igc',
-    'http://openlayers.org/en/latest/examples/data/igc/Sylvain-Dhonneur.igc',
-    'http://openlayers.org/en/latest/examples/data/igc/Ulrich-Prinz.igc',
+    // 'http://openlayers.org/en/latest/examples/data/igc/Damien-de-Baenst.igc',
+    // 'http://openlayers.org/en/latest/examples/data/igc/Tom-Payne.igc',
+    // 'http://openlayers.org/en/latest/examples/data/igc/Sylvain-Dhonneur.igc',
+    // 'http://openlayers.org/en/latest/examples/data/igc/Ulrich-Prinz.igc',
   ];
+  // Display data
+  currentPilot: string;
+  currentLatitude: string;
+  currentLongitude: string;
+  currentDate: string;
+  infosSubscription: Subscription;
 
-  constructor() { }
+  constructor(private mvs: MapViewService) { }
 
   ngOnInit() {
-    // Tests colors
-    const colors = {
-      'Clement Latour': 'rgba(0, 0, 255, 0.7)',
-      'Damien de Baesnt': 'rgba(0, 215, 255, 0.7)',
-      'Sylvain Dhonneur': 'rgba(0, 165, 255, 0.7)',
-      'Tom Payne': 'rgba(0, 255, 255, 0.7)',
-      'Ulrich Prinz': 'rgba(0, 215, 255, 0.7)'
-    };
-
-    // Load colors for each pilot, for test purposes
-    const styleCache = {};
-    const styleFunction = function(feature) {
-      const color = colors[feature.get('PLT')];
-      let myStyle = styleCache[color];
-      if (!myStyle) {
-        myStyle = new Style({
-          stroke: new Stroke({
-            color: color,
-            width: 3
-          })
-        });
-        styleCache[color] = style;
+    // Bind variables
+    this.infosSubscription = this.mvs.infosSubject.subscribe(
+      (infos: any) => {
+        this.currentPilot = infos.pilot;
+        this.currentLatitude = infos.latitude;
+        this.currentLongitude = infos.longitude;
+        this.currentDate = infos.date;
       }
-      return style;
-    };
-
-    // Container for the tracks
-    const vectorSource = new VectorSource();
-
-    // Load features inside the source container
-    const igcFormat = new IGC();
-    for (let i = 0; i < this.igcUrls.length; ++i) {
-      this.get(this.igcUrls[i], function(data) {
-        const features = igcFormat.readFeatures(data,
-          {featureProjection: 'EPSG:3857'});
-        vectorSource.addFeatures(features);
-      });
-    }
-
-    // From sample code, not usefull
-    const time = {
-      start: Infinity,
-      stop: -Infinity,
-      duration: 0
-    };
-    vectorSource.on('addfeature', function(event) {
-      const geometry = event.feature.getGeometry();
-      time.start = Math.min(time.start, geometry.getFirstCoordinate()[2]);
-      time.stop = Math.max(time.stop, geometry.getLastCoordinate()[2]);
-      time.duration = time.stop - time.start;
-    });
-
-    // Map creation
-    const map = new Map({
-      layers: [
-        new TileLayer({ // Set map background from OSM
-          source: new OSM()
-        }),
-        new VectorLayer({ // Add layer for the tracks
-          source: vectorSource,
-          style: styleFunction
-        })
-      ],
-      target: 'map',
-      controls: defaultControls({ // Temporarily using default map controls
-        attributionOptions: {
-          collapsible: false
-        }
-      }),
-      view: new View({ // Set the center position of the map view and the zoom level
-        center: [703365.7089403362, 5714629.865071137],
-        zoom: 9
-      })
-    });
-
-    // Select the closest data point from the mouse cursor
-    let point = null;
-    let line = null;
-    const displaySnap = function(coordinate) {
-      const closestFeature = vectorSource.getClosestFeatureToCoordinate(coordinate);
-      const info = document.getElementById('info');
-      const pilot = document.getElementById('pilot');
-      if (closestFeature === null) {
-        point = null;
-        line = null;
-        info.innerHTML = '&nbsp;';
-      } else {
-        const geometry = closestFeature.getGeometry();
-        const closestPoint = geometry.getClosestPoint(coordinate);
-        if (point === null) {
-          point = new Point(closestPoint);
-        } else {
-          point.setCoordinates(closestPoint);
-        }
-        const date = new Date(closestPoint[2] * 1000);
-        pilot.innerHTML = 'Pilot: ' + closestFeature.get('PLT');
-        info.innerHTML =
-          closestFeature.get('PLT') + ' (' + date.toUTCString() + ')';
-        const coordinates = [coordinate, [closestPoint[0], closestPoint[1]]];
-        if (line === null) {
-          line = new LineString(coordinates);
-        } else {
-          line.setCoordinates(coordinates);
-        }
-      }
-      map.render();
-    };
-
-    // Event triggered each time the mouse moves over the map view
-    map.on('pointermove', function(evt) {
-      if (evt.dragging) {
-        return;
-      }
-      const coordinate = map.getEventCoordinate(evt.originalEvent);
-      displaySnap(coordinate);
-    });
-
-    // Event triggered each time the map view is clicked
-    map.on('click', function(evt) {
-      displaySnap(evt.coordinate);
-    });
-
-    // Draw geometry on the closest point
-    const stroke = new Stroke({
-      color: 'rgba(255,0,0,0.9)',
-      width: 1
-    });
-    const style = new Style({
-      stroke: stroke,
-      image: new CircleStyle({
-        radius: 5,
-        fill: null,
-        stroke: stroke
-      })
-    });
-    map.on('postcompose', function(evt) {
-      const vectorContext = evt.vectorContext;
-      vectorContext.setStyle(style);
-      if (point !== null) {
-        vectorContext.drawGeometry(point);
-      }
-      if (line !== null) {
-        vectorContext.drawGeometry(line);
-      }
-    });
-
-    // Create a new Layer
-    const featureOverlay = new VectorLayer({
-      source: new VectorSource(),
-      map: map,
-      style: new Style({
-        image: new CircleStyle({
-          radius: 5,
-          fill: new Fill({
-            color: 'rgba(255,0,0,0.9)'
-          })
-        })
-      })
-    });
-  }
-
-  // GET html request
-  get(url, callback) {
-    const client = new XMLHttpRequest();
-    client.open('GET', url);
-    client.onload = function () {
-      callback(client.responseText);
-    };
-    client.send();
+    );
+    this.mvs.emitInfos();
+    // Setup map view
+    this.mvs.initMap();
+    this.mvs.loadTrack(this.igcUrls);
+    this.mvs.setupEvents();
   }
 }
--- src/app/services/map-view.service.ts
+++ src/app/services/map-view.service.ts
@@ -1,9 +1,183 @@
 import { Injectable } from '@angular/core';
 
+import Feature from 'ol/Feature';
+import Map from 'ol/Map';
+import View from 'ol/View';
+import {defaults as defaultControls} from 'ol/control';
+import IGC from 'ol/format/IGC';
+import {LineString, Point} from 'ol/geom';
+import {Tile as TileLayer, Vector as VectorLayer} from 'ol/layer';
+import OSM, {ATTRIBUTION} from 'ol/source/OSM';
+import VectorSource from 'ol/source/Vector';
+import {fromLonLat} from 'ol/proj';
+import {Circle as CircleStyle, Fill, Stroke, Style} from 'ol/style';
+import {Subject} from 'rxjs';
+
 @Injectable({
   providedIn: 'root'
 })
 export class MapViewService {
 
+  // Define base colors
+  COLORS = {
+    red: [255, 0, 0, 0.9],
+    green: [255, 0, 0, 0.9],
+    blue: [255, 0, 0, 0.9]
+  };
+  // Main attributes
+  map: Map;
+  view: View;
+  vectorSource: VectorSource;
+  // Data for closest point
+  infos = {
+    pilot: '',
+    latitude: '',
+    longitude: '',
+    date: Date()
+  };
+  infosSubject = new Subject<any>();
+  overlayShapes = {
+    line: null,
+    point: null
+  };
+  // Style for overlay
+  stroke = new Stroke({
+    color: this.COLORS.red,
+    width: 1
+  });
+  style = new Style({
+    stroke: this.stroke,
+    image: new CircleStyle({
+      radius: 5,
+      fill: null,
+      stroke: this.stroke
+    })
+  });
+
   constructor() { }
+
+  initMap() {
+    // Init View
+    this.view = new View({ // Set the center position of the map view and the zoom level
+      center: [0, 0],
+      zoom: 9
+    });
+
+    // Container for the tracks
+    this.vectorSource = new VectorSource();
+    this.vectorSource.on('addfeature', (event) => {
+      const geometry = event.feature.getGeometry();
+      this.view.fit(geometry);
+    });
+
+    // Map creation
+    this.map = new Map({
+      layers: [
+        new TileLayer({ // Set map background from OSM
+          source: new OSM()
+        }),
+        new VectorLayer({ // Add layer for the tracks
+          source: this.vectorSource,
+          style: new Style({
+            stroke: new Stroke({
+              color: this.COLORS.red,
+              width: 2
+            })
+          })
+        })
+      ],
+      target: 'map',
+      controls: defaultControls({ // Temporarily using default map controls
+        attributionOptions: {
+          collapsible: false
+        }
+      }),
+      view: this.view
+    });
+  }
+
+  loadTrack(igcUrls) {
+    // Load features inside the source container
+    const igcFormat = new IGC('gps');
+    for (let i = 0; i < igcUrls.length; ++i) {
+      this.get(igcUrls[i], (data) => {
+        const features = igcFormat.readFeatures(data,
+          {featureProjection: 'EPSG:3857'});
+        this.vectorSource.addFeatures(features);
+      });
+    }
+  }
+
+  setupEvents() {
+    // Event triggered each time the mouse moves over the map view
+    this.map.on('pointermove', (evt) => {
+      if (evt.dragging) {
+        return;
+      }
+      const coordinate = this.map.getEventCoordinate(evt.originalEvent);
+      this.displaySnap(coordinate);
+    });
+
+    // Event triggered each time the map view is clicked
+    this.map.on('click', function(evt) {
+      // Do stuff
+    });
+    // Draw geometry on the closest point
+    this.map.on('postcompose', (evt) => {
+      const vectorContext = evt.vectorContext;
+      vectorContext.setStyle(this.style);
+      if (this.overlayShapes.point !== null) {
+        vectorContext.drawGeometry(this.overlayShapes.point);
+      }
+      if (this.overlayShapes.line !== null) {
+        vectorContext.drawGeometry(this.overlayShapes.line);
+      }
+    });
+  }
+
+  displaySnap(coordinate) {
+    // Fetch closest track from mouse coords
+    const closestFeature = this.vectorSource.getClosestFeatureToCoordinate(coordinate);
+    if (closestFeature === null) {
+      this.overlayShapes.point = null;
+      this.overlayShapes.line = null;
+    } else {
+      // Fetch closest point from mouse coords
+      const geometry = closestFeature.getGeometry();
+      const closestPoint = geometry.getClosestPoint(coordinate);
+      if (this.overlayShapes.point === null) {
+        this.overlayShapes.point = new Point(closestPoint);
+      } else {
+        this.overlayShapes.point.setCoordinates(closestPoint);
+      }
+      // Update data of the closest point
+      this.infos.pilot = closestFeature.get('PLT');
+      this.infos.latitude = closestPoint[0];
+      this.infos.longitude = closestPoint[1];
+      this.infos.date = new Date(closestPoint[2] * 1000).toUTCString();
+      this.emitInfos();
+      // Update shapes to draw for the closest point
+      const coordinates = [coordinate, [closestPoint[0], closestPoint[1]]];
+      if (this.overlayShapes.line === null) {
+        this.overlayShapes.line = new LineString(coordinates);
+      } else {
+        this.overlayShapes.line.setCoordinates(coordinates);
+      }
+    }
+    this.map.render();
+  }
+
+  // GET html request
+  get(url, callback) {
+    const client = new XMLHttpRequest();
+    client.open('GET', url);
+    client.onload = () => {
+      callback(client.responseText);
+    };
+    client.send();
+  }
+
+  emitInfos() {
+    this.infosSubject.next(this.infos);
+  }
 }
